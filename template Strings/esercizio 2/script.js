23316145f941b03601d66cf7bd0c0121b74db49a
JSter91 <129691071+JSter91@users.noreply.github.com>
2023-11-24 17:17:25 +0100

    template strings

---
+++ template Strings/esercizio 2/script.js
@@ -0,0 +1,10 @@
+// Utilizza una template string per creare una stringa su più righe che includa interruzioni di riga.
+
+let string = `Nella quiete della notte,
+la luna svela il suo splendore argentato.
+Un sottile vento accarezza le foglie degli alberi, 
+mentre stelle scintillanti dipingono il cielo. 
+La città si addormenta, avvolta da un manto di tranquillità.
+La magia della notte risveglia sogni e promesse nell'aria serena.`;
+
+console.log(string);
