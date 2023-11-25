7c4aa23b5083519d96284fdab71f4d65f0bd7329
JSter91 <129691071+JSter91@users.noreply.github.com>
2023-11-25 14:06:49 +0100

    destrutturazione 4 modified

--- destrutturazione/esercizio 4/script.js
+++ destrutturazione/esercizio 4/script.js
@@ -5,5 +5,5 @@ let b = 10;
 
 [a, b] = [b, a];
 
-console.log("Nuovo valore di a:", a);
-console.log("Nuovo valore di b:", b);
+console.log(`Nuovo valore di a: ${a}`);
+console.log(`Nuovo valore di b: ${b}`);
