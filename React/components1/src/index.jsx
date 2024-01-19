41c3bf5eb0ade7cbac34f04e3b87c38743d9d422
salvatore <marottasalvatore@gmail.com>
2024-01-19 14:32:53 +0100

    components 1

--- React/components1/src/index.jsx
+++ React/components1/src/index.jsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client';
-import App from './App';
+import Hello from './Hello';
 
-const root = createRoot(document.getElementById('app'));
-root.render(<App />);
+const root = createRoot(document.getElementById('root'));
+root.render(<Hello />);
