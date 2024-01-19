56339ca310adfa8f906aceef7cfe7f9a5204f014
salvatore <marottasalvatore@gmail.com>
2024-01-19 15:31:14 +0100

    components2-3

---
+++ React/components2/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Hello from './Hello';
+import Message from './Message';
+
+const App = () => {
+  return (
+    <div>
+      <Hello />
+      <Message/>
+    </div>
+  );
+};
+
+export default App;
