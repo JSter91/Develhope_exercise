cfdf796c302c7230fb89985ccb583459647817b9
salvatore <marottasalvatore@gmail.com>
2024-01-20 15:15:46 +0100

    props2

---
+++ React/props2/src/Welcome.jsx
@@ -0,0 +1,12 @@
+function Welcome({ name, age }) {
+  return (
+    <>
+      <p>
+        Welcome <strong> {name}</strong>!
+      </p>
+      <p>Your age is {age}</p>
+    </>
+  );
+}
+
+export default Welcome;
