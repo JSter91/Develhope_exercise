be0322bcdb3d0244c12d64361fe5940cbb075a2a
salvatore <marottasalvatore@gmail.com>
2024-01-21 18:17:36 +0100

    state 1-2

--- React/state/src/Counter.jsx
+++ React/state/src/Counter.jsx
@@ -16,3 +16,14 @@ function Counter (){
 }
 
 export default Counter
+
+/* 
+When calling "setter" function to increment the counter, 
+should the parameter be a function or an immediate value? Why?
+ 
+
+In most cases, for simple state increments like in your counter example, 
+either approach works. However, when dealing with more complex state updates or 
+when the new state depends on the previous state, using a function is recommended
+ to ensure correctness and avoid potential issues.
+  */
