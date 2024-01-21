d6952618b85c16fb9d55ac629099998f4ed567f3
salvatore <marottasalvatore@gmail.com>
2024-01-21 17:52:28 +0100

    state

---
+++ React/state/src/Counter.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react"
+
+function Counter (){
+    const [count, setCount] = useState(0)
+
+    function handleCounterIncrement(){
+        setCount(count + 1)
+    }
+
+    return(
+        <>
+        <h2>{count}</h2>
+        <button onClick={handleCounterIncrement}>premi</button>
+        </>
+    )
+}
+
+export default Counter
