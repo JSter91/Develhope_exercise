189605d216d523f0196da0a65266f726d776b9eb
salvatore <marottasalvatore@gmail.com>
2024-01-24 17:17:34 +0100

    controlled forms 3

---
+++ React/controlled forms/controlled3/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Login from './Login'
+function App() {
+  const handleLogin = (userData) => {
+    console.log(userData);
+  }
+  
+
+  return (
+    <>
+    <Login onLogin={handleLogin}></Login>
+    </>
+  )
+}
+
+export default App
