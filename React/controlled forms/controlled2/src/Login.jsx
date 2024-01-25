3b3ade650eb5e5704cc249de116f2a49d8095043
salvatore <marottasalvatore@gmail.com>
2024-01-25 17:40:12 +0100

    bug fixed

--- React/controlled forms/controlled2/src/Login.jsx
+++ React/controlled forms/controlled2/src/Login.jsx
@@ -3,7 +3,7 @@ function Login() {
   const [data, setData] = useState({
     username: "",
     password: "",
-    session: false
+    session: false,
   });
 
   function handleInputChange(event) {
@@ -22,35 +22,37 @@ function Login() {
 
   return (
     <>
-      <h1>Login</h1>
-      <input
-        name="username"
-        value={data.username}
-        type="text"
-        placeholder="username"
-        onChange={handleInputChange}
-      />
-      <br />
-      <input
-        name="password"
-        value={data.password}
-        type="password"
-        placeholder="password"
-        onChange={handleInputChange}
-      />
-      <br />
-      <label htmlFor="in">remember</label>
-      <input
-        name="session"
-        id="in"
-        type="checkbox"
-        checked={data.session}
-        onChange={handleInputChange}
-      ></input>
-      <br />
-      <br />
+      <form>
+        <h1>Login</h1>
+        <input
+          name="username"
+          value={data.username}
+          type="text"
+          placeholder="username"
+          onChange={handleInputChange}
+        />
+        <br />
+        <input
+          name="password"
+          value={data.password}
+          type="password"
+          placeholder="password"
+          onChange={handleInputChange}
+        />
+        <br />
+        <label htmlFor="in">remember</label>
+        <input
+          name="session"
+          id="in"
+          type="checkbox"
+          checked={data.session}
+          onChange={handleInputChange}
+        ></input>
+        <br />
+        <br />
 
-      <pre>{JSON.stringify(data, null, 1)}</pre>
+        <pre>{JSON.stringify(data, null, 1)}</pre>
+      </form>
     </>
   );
 }
