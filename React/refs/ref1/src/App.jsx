cd79cb71c897a116905945eb4e2b41d94a3589af
salvatore <marottasalvatore@gmail.com>
2024-01-25 15:15:38 +0100

    ref 1

---
+++ React/refs/ref1/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import FocusableInput from './FocusableInput'
+
+function App() {
+
+  return (
+    <>
+    <FocusableInput></FocusableInput>
+    </>
+  )
+}
+
+export default App
