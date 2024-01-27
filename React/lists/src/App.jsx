9bab8b179266c68d0560ff0a57711d7c606ad8ff
salvatore <marottasalvatore@gmail.com>
2024-01-27 19:29:51 +0100

    lists

---
+++ React/lists/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+import { Colors } from './Colors'
+
+
+function App() {
+  const colorList = [
+    { id: 1, name: "Red" },
+    { id: 2, name: "Green" },
+    { id: 3, name: "Blue" },
+  ];
+ 
+  return (
+    <div>
+      <h1>Lista dei Colori:</h1>
+      <Colors colorList={colorList} />
+    </div>
+  );
+
+}
+
+export default App
