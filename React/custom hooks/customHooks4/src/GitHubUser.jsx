5fa08056b05a68e766b5da9bebba4f4f23588d1a
salvatore <marottasalvatore@gmail.com>
2024-02-06 12:14:18 +0100

    custom-hooks4

---
+++ React/custom hooks/customHooks4/src/GitHubUser.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect } from "react";
+import useGitHubUser from "./useGitHubUser";
+
+function GitHubUser({ username }) {
+  const { user, loading, error, fetchData } = useGitHubUser();
+
+  useEffect(() => {
+    if (username) {
+      fetchData(username);
+    }
+  }, [username]);
+
+  return (
+    <>
+      {loading && <p>Caricamento...</p>}
+      {error && <p>Si è verificato un errore: {error}</p>}
+      {user && (
+        <div className="card-user">
+          <h3>{user.name}</h3>
+          <p>{user.login}</p>
+          <div>
+            <img className="avatar" src={user.avatar_url} alt="avatar" />
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default GitHubUser;
