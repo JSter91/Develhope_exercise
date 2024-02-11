bccaccbef63874f46af1129862a13bbada59b22b
salvatore <marottasalvatore@gmail.com>
2024-02-11 16:33:52 +0100

    use-SWR3-4

--- React/use-SWR/use-SWR3/src/useGitHubUser.jsx
+++ React/use-SWR/use-SWR3/src/useGitHubUser.jsx
@@ -5,16 +5,11 @@ const fetcher = (url) => fetch(url).then((response) => response.json());
 function useGitHubUser({ username }) {
   const { data, error, mutate } = useSWR(username ? `https://api.github.com/users/${username}` : null, fetcher);
 
-  function handleRefresh(){
-    mutate()
-  }
-
-
   return {
     user: data,
     error: error,
     isLoading: username && !data && !error,
-    onRefresh: handleRefresh
+    onRefresh: mutate
   };
 }
 
