87bfa6530537412de26cc3e76bcde3dda5775a58
salvatore <marottasalvatore@gmail.com>
2024-04-13 10:28:45 +0200

    replace react

--- Node/REPL/expolre_REPL.js
+++
@@ -1,6 +0,0 @@
-const crypto = require('crypto');
-
-console.log(Object.keys(crypto));
-
-const randomBytes = crypto.randomBytes(8).toString('hex');
-console.log('Random ID:', randomBytes);
--- Node/create_sum_script/sum.js
+++
@@ -1,5 +0,0 @@
-const sum = (a, b) => a + b;
-
-const result = sum(5, 6);
-
-console.log(result);
